1e069561e32b17b6ad752145c5106720f74e23d9
Nguyễn Trọng Minh Hồng Phước <39150891+phuocntmh@users.noreply.github.com>
2019-05-18 15:46:05 +0700

    update

--- js/script.js
+++ js/script.js
@@ -5,24 +5,27 @@ frm.submit(function (e) {
         e.preventDefault();
         alert("Vui lòng kiểm tra biểu mẫu. Error: Please check the Recaptcha.");
     }
-    $.ajax({
-        type: 'GET',
-        beforeSend: function(){
-            $("input").prop('disabled', true);
-            $("#submit-form").attr("disabled", true);
-        },
-        url: 'https://script.google.com/macros/s/AKfycbxBwosreW7JvP2F0kMnIpmSgNT4Pp7yPZdzBqg1cH-P6vpG3gI/exec',
-        data: frm.serialize(),
-        success: function (data) {
-            $('#SuccessModal').modal('show');
-        },
-        error: function (data) {
-            $('#FailModal').modal('show');
-        },
-        complete: function(){
-            $("input").prop('disabled', false);
-            $("#submit-form").attr("disabled", false);
-            $('#register-form')[0].reset();
-        }
-    });
+    else {
+        e.preventDefault();
+        $.ajax({
+            type: 'GET',
+            beforeSend: function(){
+                $("input").prop('disabled', true);
+                $("#submit-form").attr("disabled", true);
+            },
+            url: 'https://script.google.com/macros/s/AKfycbxBwosreW7JvP2F0kMnIpmSgNT4Pp7yPZdzBqg1cH-P6vpG3gI/exec',
+            data: frm.serialize(),
+            success: function (data) {
+                $('#SuccessModal').modal('show');
+            },
+            error: function (data) {
+                $('#FailModal').modal('show');
+            },
+            complete: function(){
+                $("input").prop('disabled', false);
+                $("#submit-form").attr("disabled", false);
+                $('#register-form')[0].reset();
+            }
+        });
+    }
 });
